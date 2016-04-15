283f0187718c2d9df1cf775b5cb592e82e0a91e6
Laura Salinas <laurabug2010@gmail.com>
2016-04-15 10:48:16 -0400

    commiting react

--- js/app.js
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var List = require('./Footer.jsx');
-
-module.exports = React.createClass({
-    render: function() {
-        return <ul>{this.props.action}<li>{this.props.name}</li></ul>;
-    }
-})
